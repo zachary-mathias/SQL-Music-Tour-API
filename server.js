3651d375637327ec53abb749896dae0c1c09c2a0
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-07-02 12:17:41 -0400

    added connection to the server.js with try catch to ensure it is working

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({

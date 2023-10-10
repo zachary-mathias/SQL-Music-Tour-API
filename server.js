86893d2b261643d632f52c99b7e3e35f40bd0f85
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-10-10 19:41:58 -0400

    everything complete and working correctly

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// DEPENDENCIES
 const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
+const bands = require('./controllers/bands_controller')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
 
 // ROOT
 app.get('/', (req, res) => {
@@ -15,6 +14,16 @@ app.get('/', (req, res) => {
         message: 'Welcome to the Tour API'
     })
 })
+
+// CONTROLLERS  
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
